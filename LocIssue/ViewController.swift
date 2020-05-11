8e3e74c9857e5590719a81643eeb663640d677b2
Sumuhan <sumuhan@gmail.com>
2020-05-11 12:02:32 +0100

    Initial Commit

---
+++ LocIssue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocIssue
+//
+//  Created by sumuhan umamaheswarampillai on 11/05/2020.
+//  Copyright © 2020 sumuhan umamaheswarampillai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
